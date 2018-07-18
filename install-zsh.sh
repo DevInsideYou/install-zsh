3afbb33ee6aa28b38eb32e68b5fe8646ff69a00b
Vladyslav Pekker <agilesteel@gmail.com>
2018-07-18 10:24:38 +0200

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -9,11 +9,11 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 # change theme to "intheloop"
 sed -i '/ZSH_THEME="robbyrussell"/c\ZSH_THEME="intheloop"' ~/.zshrc
 
-# change default shell to zsh
-chsh -s /bin/zsh
-
 # display additional info
 echo "Don't forget to change the gnome-terminal command to zsh in the profile preferences."
+
+# change default shell to zsh
+chsh -s /bin/zsh
 
 # run zsh
 zsh
